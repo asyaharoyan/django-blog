f59804798a663bd8c4296889e3f18821206bc7d0
Asya Haroyan <alharoyan@gmail.com>
2024-02-27 16:25:28 +0000

    Add database

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")  #'django-insecure-de(62kf$e9!!zz6r&%d5f9n-7)^+pbjqwplw#**^3bxgtqe46p'
+SECRET_KEY = os.environ.get("SECRET_KEY")  
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-asyaharoyan-djangoblog-79w6yejvm3t.ws-eu108.gitpod.io', '.herokuapp.com']
 
@@ -89,6 +89,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
