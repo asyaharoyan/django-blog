cf30241024087abac8cebbcbe4d1284dd555db1a
Asya Haroyan <alharoyan@gmail.com>
2024-03-05 10:21:09 +0000

    add about app

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")  
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-asyaharoyan-djangoblog-79w6yejvm3t.ws-eu108.gitpod.io', '.herokuapp.com']
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'django_summernote',
+    'about',
 ]
 
 MIDDLEWARE = [
--- codestar/urls.py
+++ codestar/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 # from blog.views import my_blog
 
 urlpatterns = [
-    path('', include("blog.urls"), name='blog-urls'),
+    path('about/', include("about.urls"), name='about-urls'),
     path('admin/', admin.site.urls),
+    path('', include("blog.urls"), name='blog-urls'),
     path('summernote/', include('django_summernote.urls')),
 ]
