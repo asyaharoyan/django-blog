3c04ef9a3951bd18f8c512f109ef422bb879ba8b
Asya Haroyan <alharoyan@gmail.com>
2024-03-26 15:31:59 +0000

    Add tests

---
+++ about/test_views.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.urls import reverse
+from .models import About
+from .forms import CollaborateForm
+
+class TestAboutView(TestCase):
+    def setUp(self):
+        """Creates about me content"""
+        self.about_content = About(
+            title="About me", content="This is about me")
+        self.about_content.save()
+
+    def test_render_about_page_with_collaborate_form(self):
+        """
+        Verifies get request for about me containing a collaboration form
+        """
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"About me", response.content)
+        self.assertIsInstance(
+            response.context['collaborate_form'], CollaborateForm)
+
+    def test_successful_collaboration_submission(self):
+        """Test for collaboration request"""
+        post_data = {
+        'name': 'test name',
+        'email': 'test@email.com',
+        'message': 'test message'
+    }
+        response = self.client.post(reverse('about'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'Collaboration request received! I endeavour to respond within 2 working days.',
+            response.content
+        )
